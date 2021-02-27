af5efab4d05f864fb4e3ca3aece82113038a9a32
Giacomo Nanni <gn.nanni@gmail.com>
2021-02-27 21:29:45 +0100

    feat fonts + seo stuff

--- www/site/plugins/site/pageMethods.php
+++ www/site/plugins/site/pageMethods.php
@@ -2,22 +2,23 @@
 
 return [
 
-  'createDescription' => function () {
-      $description = "";
-      return $description;
-  },
+    'createDescription' => function () {
+        $description = $this->site()->descriptionSEO()->smartypants();
+        return $description;
+    },
 
-  'createTitle' => function () {
-      if ($this->template() == 'home'):
-          return $this->site()->title()->smartypants();
-      elseif ($this->template() == 'error'):
-          return $this->site()->title()->smartypants() . ' | This page is not available.';
-      else:
-          return $this->site()->title()->smartypants() . ' | ' . $this->title()->smartypants();
-      endif;
-  },
+    'createTitle' => function () {
+        if ($this->template() == 'home') :
+            return $this->site()->title()->smartypants();
+        elseif ($this->template() == 'error') :
+            return $this->site()->title()->smartypants() . ' | This page is not available.';
+        else :
+            return $this->site()->title()->smartypants() . ' | ' . $this->title()->smartypants();
+        endif;
+    },
 
-  'createImage' => function () {
-      // return $image->crop(1200, 630, 'center')->url();
-  }
+    'createImage' => function () {
+        $image = $this->site()->imageSEO()->toFile();
+        return $image->crop(1200, 630, 'center')->url();
+    }
 ];
